88bf7b0c1047204c2d3eed07dd6d4a4ab1f62957
Minh Dang <dang14072k2@gmail.com>
2025-10-05 22:35:35 +0700

    update api url to build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_BASE_URL: "https://localhost:7241/api/v1/",
+    NEXT_PUBLIC_API_BASE_URL: "http://localhost:1472/api/v1/",
     NEXT_PUBLIC_ACCESS_TOKEN_KEY: "access_token",
     NEXT_PUBLIC_REDIRECT_LOGIN_DELAY: "1"
   },
